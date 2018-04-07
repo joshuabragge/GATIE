fe04bfe23a83891698a931ccc4df60a7fce21ab8
Josh <joshuabragge@hotmail.com>
2018-04-07 17:55:33 -0600

    fixed shared_state parameters not being passed to manager.ProcessManager

--- run.py
+++ run.py
@@ -6,17 +6,19 @@ from gaite import manager
 
 def main():
     parser = argparse.ArgumentParser(description='Read google analytics and send results to recruiters')
-    parser.add_argument('-e', '--env', choices=['test', 'prod'], help='test or prod', default='test')
+    parser.add_argument('-e', '--env', choices=['test', 'prod'], help='test or prod', default='prod')
     parser.add_argument('-v', '--verbosity', help="increase verbosity for troubleshooting", action='store_true')
     parser.add_argument('-r', '--recruiter', help="specify a single recruiter to act upon")
     args = parser.parse_args()
 
     env = environment.Env()
-    env._shared_state['env'] = args.env
+    env.shared_state['env'] = args.env
     if args.verbosity:
-        env._shared_state['verbosity'] = True
+        env.shared_state['verbosity'] = True
+    if args.recruiter is not None:
+        env.shared_state['recruiter'] = args.recruiter
 
-    runner = manager.ProcessManager()
+    runner = manager.ProcessManager(env)
 
     runner.run()
 
