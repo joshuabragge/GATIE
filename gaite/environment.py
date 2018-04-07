f1060ff549082dd6597f1862a772bb1baf702218
Josh <joshuabragge@hotmail.com>
2018-04-07 17:54:32 -0600

    added more shared_state variables for runtime start and end date parameters

--- gaite/environment.py
+++ gaite/environment.py
@@ -1,9 +1,10 @@
 
-
-class Env:
+class Env(object):
 
     def __init__(self):
-        self._shared_state = dict()
-        self._shared_state['env'] = None
-        self._shared_state['verbosity'] = False
-        self._shared_state['recruiter'] = None
+        self.shared_state = dict()
+        self.shared_state['env'] = None
+        self.shared_state['verbosity'] = False
+        self.shared_state['recruiter'] = None
+        self.shared_state['start_date'] = None
+        self.shared_state['end_date'] = None
